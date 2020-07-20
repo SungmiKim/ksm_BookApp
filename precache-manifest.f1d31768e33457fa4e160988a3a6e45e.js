fee6fcf260cb4399152d1fa4326d4de5ef200727
SungmiKim <62002100+SungmiKim@users.noreply.github.com>
2020-07-20 14:14:40 +0900

    Updates

---
+++ precache-manifest.f1d31768e33457fa4e160988a3a6e45e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0eca04779ae77401ef5a23d6409add",
+    "url": "/ksm_bookapp/index.html"
+  },
+  {
+    "revision": "ce6e0e241cb648962060",
+    "url": "/ksm_bookapp/static/css/main.9ceba25d.chunk.css"
+  },
+  {
+    "revision": "67939a72f89f2d0d4674",
+    "url": "/ksm_bookapp/static/js/2.9a17b7d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ksm_bookapp/static/js/2.9a17b7d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6e0e241cb648962060",
+    "url": "/ksm_bookapp/static/js/main.5f820f5f.chunk.js"
+  },
+  {
+    "revision": "8ca32926133214c88e5d",
+    "url": "/ksm_bookapp/static/js/runtime-main.dccc6b0e.js"
+  },
+  {
+    "revision": "7e32b167c04afd8d698a0b53b28d2c13",
+    "url": "/ksm_bookapp/static/media/wall-bookshelf.7e32b167.png"
+  }
+]);
